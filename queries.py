55f3852eddce5f4d1a631b5fb559bbcf9f939950
robert1ridley <robert.n.ridley@gmail.com>
2019-08-08 11:38:25 +0800

    add some sparql queries.

--- queries.py
+++ queries.py
@@ -105,10 +105,192 @@ class Queries:
       else:
         raise NotImplementedError
 
+  def about_character(self, subj):
+    qres = self.g.query(
+      """SELECT DISTINCT ?charname ?intro
+      WHERE{
+          ?person foaf:intro ?intro .
+          ?person foaf:name ?charname .
+      }""")
+
+    for row in qres:
+      name = row[0]
+      intro = row[1]
+
+      if subj:
+        if str(name) == subj:
+          print("Some info about {}: {}".format(subj, intro))
+      else:
+        raise NotImplementedError
+
+  def fictional(self, subj, fictional):
+    qres = self.g.query(
+      """SELECT DISTINCT ?charname ?fictional
+      WHERE{
+          ?person foaf:fictional ?fictional .
+          ?person foaf:name ?charname .
+      }
+      """)
+
+    for row in qres:
+      name = row[0]
+      fict = row[1]
+
+      if subj:
+        if str(name) == subj:
+          print("{} was {}.".format(name, fict))
+      elif fictional:
+        if str(fict) == fictional:
+          print("{} was {}.".format(name, fict))
+
+  def which_chapter(self, subj, chapt):
+    qres = self.g.query(
+      """SELECT DISTINCT ?eventname ?chapter
+      WHERE{
+          ?event foaf:chapter ?chapter .
+          ?event foaf:event_name ?eventname .
+      }""")
+
+    for row in qres:
+      event_name = row[0]
+      chapter = row[1]
+
+      if subj:
+        if str(event_name) == subj:
+          print("{} is in {}.".format(event_name, chapter))
+
+      elif chapt:
+        if str(chapter) == chapt:
+          print("{} is in {}.".format(event_name, chapter))
+
+  def event_info(self, subj):
+    qres = self.g.query(
+      """SELECT DISTINCT ?eventname ?info
+      WHERE{
+          ?event foaf:description ?info .
+          ?event foaf:event_name ?eventname .
+      }""")
+
+    for row in qres:
+      event_name = row[0]
+      description = row[1]
+
+      if subj:
+        if str(event_name) == subj:
+          print("Some info about {}: {}".format(event_name, description))
+
+      else:
+        raise NotImplementedError
+
+  def event_history(self, subj):
+    qres = self.g.query(
+      """SELECT DISTINCT ?eventname ?history
+      WHERE{
+          ?event foaf:history ?history .
+          ?event foaf:event_name ?eventname .
+      }""")
+
+    for row in qres:
+      event_name = row[0]
+      history = row[1]
+
+      if subj:
+        if str(event_name) == subj:
+          print("Some history about {}: {}".format(event_name, history))
+
+      else:
+        raise NotImplementedError
+
+  def event_location(self, subj, loc):
+    qres = self.g.query(
+      """SELECT DISTINCT ?eventname ?location
+      WHERE{
+          ?event foaf:location ?location .
+          ?event foaf:event_name ?eventname .
+      }""")
+
+    for row in qres:
+      event_name = row[0]
+      location = row[1]
+
+      if subj:
+        if str(event_name) == subj:
+          print("{} occurred at {}".format(event_name, location))
+      elif loc:
+        if str(location) == loc:
+          print("{} occurred at {}".format(event_name, location))
+      else:
+        raise NotImplementedError
+
+  def event_time(self, subj, year):
+    qres = self.g.query(
+      """SELECT DISTINCT ?eventname ?year
+      WHERE{
+          ?event foaf:time ?year .
+          ?event foaf:event_name ?eventname .
+      }""")
+
+    for row in qres:
+      event_name = row[0]
+      time = row[1]
+
+      if subj:
+        if str(event_name) == subj:
+          print("{} occurred in {}.".format(event_name, time))
+      elif year:
+        if str(time) == year:
+          print("{} occurred in {}.".format(event_name, time))
+      else:
+        raise NotImplementedError
+
+  def event_involved(self, subj, char):
+    qres = self.g.query(
+      """SELECT DISTINCT ?eventname ?char
+      WHERE{
+          ?event foaf:involved ?involved .
+          ?event foaf:event_name ?eventname .
+          ?involved foaf:name ?char .
+      }
+      """)
+
+    people_involved = []
+    events_involved_in = []
+    for row in qres:
+      event = row[0]
+      involved = row[1]
+
+      if subj:
+        if str(event) == subj:
+          people_involved.append(involved)
+      elif char:
+        if str(involved) == char:
+          events_involved_in.append(event)
+      else:
+        raise NotImplementedError
+
+    people_involved_string = ', '.join(people_involved)
+    events_involved_in_string = ', '.join(events_involved_in)
+    if len(people_involved) > 0:
+      print("{} involved {} people: {}".format(event, len(people_involved), people_involved_string))
+    elif len(events_involved_in) > 0:
+      print("{} involved in {} events: {}".format(char, len(events_involved_in), events_involved_in_string))
 
 if __name__ == '__main__':
     templates = Queries()
     # templates.loyalty("丁斐", None)
     # templates.birthplace(None, "豫州沛国")
     # templates.lifespan("丁仪")
-    templates.gender("丁仪", None)
+    # templates.gender("丁仪", None)
+    # templates.about_character("丁仪")
+    # templates.fictional(None, "史实人物")
+    # templates.which_chapter(None, "第17回第1段")
+    # templates.event_info("一意孤行袁术篡逆称帝")
+    # templates.event_history("一意孤行袁术篡逆称帝")
+    # templates.event_location(None, "寿春")
+    # templates.event_time(None, "196年")
+    # templates.event_involved("孙策讨要玉玺袁术不还", None)
+
+
+
+
+
