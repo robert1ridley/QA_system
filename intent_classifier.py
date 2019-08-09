1a71e4f719966218cb2031760e3c664155c7bc6c
robert1ridley <robert.n.ridley@gmail.com>
2019-08-09 15:30:34 +0800

    convolutional model completed

--- intent_classifier.py
+++ intent_classifier.py
@@ -1,5 +1,7 @@
 import random
+import numpy as np
 import jieba.posseg as pseg
+import deep_learning_models.convolutional_network as deep_models
 
 def load_data(filename):
     text_file = open(filename)
@@ -20,6 +22,7 @@ def split_data(data):
 
 def get_labels(data):
     x_data, y_data = [], []
+    largest_label = -1
     for item in data:
         item = item.strip()
         splits = item.split()
@@ -27,7 +30,9 @@ def get_labels(data):
         y = splits[1]
         x_data.append(x.strip())
         y_data.append(y.strip())
-    return x_data, y_data
+        if int(y) > largest_label:
+            largest_label = int(y)
+    return x_data, y_data, largest_label
 
 
 def generate_vocabulary(data):
@@ -61,18 +66,71 @@ def indicize_data(vocab_dict, data):
     return all_indices, max_sent
 
 
+def padd_sentences(sentences, max_sent):
+    sentences_array = np.zeros([len(sentences), max_sent], dtype='int')
+    for i in range(len(sentences)):
+        for j in range(len(sentences[i])):
+            sentences_array[i, j] = int(sentences[i][j])
+    return sentences_array
+
+
+def padd_labels(labels, label_num):
+    label_array = np.zeros([len(labels), label_num], dtype='int')
+    for i in range(len(labels)):
+        label_array[i, int(labels[i])-1] = 1
+    return label_array
+
+# def padd_labels(labels):
+#     label_array = np.zeros([len(labels), 1], dtype='int')
+#     for i in range(len(labels)):
+#         label_array[i, 0] = int(labels[i])
+#     return label_array
+
+
 if __name__ == '__main__':
     filename = 'data/training.txt'
     all_data = load_data(filename)
     train, dev, test = split_data(all_data)
-    x_train, y_train = get_labels(train)
-    x_dev, y_dev = get_labels(dev)
-    x_test, y_test = get_labels(test)
+    x_train, y_train, train_highest_label = get_labels(train)
+    x_dev, y_dev, dev_highest_label = get_labels(dev)
+    x_test, y_test, test_highest_label = get_labels(test)
     voc_dict = generate_vocabulary(train)
     x_train, max_train_sentence = indicize_data(voc_dict, x_train)
     x_dev, max_dev_sentence = indicize_data(voc_dict, x_dev)
     x_test, max_test_sentence = indicize_data(voc_dict, x_test)
     max_sentence = max(max_train_sentence, max_dev_sentence, max_test_sentence)
-    
+    highest_label = max(train_highest_label, dev_highest_label, test_highest_label)
+    x_train = padd_sentences(x_train, max_sentence)
+    x_dev = padd_sentences(x_dev, max_sentence)
+    x_test = padd_sentences(x_test, max_sentence)
+
+    y_train = padd_labels(y_train, highest_label)
+    y_dev = padd_labels(y_dev, highest_label)
+    y_test = padd_labels(y_test, highest_label)
+
+    # y_train = padd_labels(y_train)
+    # y_dev = padd_labels(y_dev)
+    # y_test = padd_labels(y_test)
+
+    model = deep_models.build_convolutional_network(max_sentence, len(voc_dict), highest_label)
+
+    model.fit(x_train, y_train, epochs=1)
+
+    dev_loss, dev_acc = model.evaluate(x_dev, y_dev)
+    print('Test accuracy:', dev_acc)
+    predictions = model.predict(x_test)
+
+    for i in range(10):
+        for j in range(len(predictions[i])):
+            if predictions[i, j] > 0.5:
+                predictions[i, j] = 1
+            else:
+                predictions[i, j] = 0
+
+    print(predictions[:10])
+    print(y_test[:10])
+
+
+
 
 
